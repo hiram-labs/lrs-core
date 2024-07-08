224601927058245876a709acd042acf888fd053a
nanacnote <adjeibohyen@hotmail.co.uk>
2024-07-08 22:44:25 +0100

    refactor: update statement queue doutil to use async callback

--- worker/src/handlers/statement/listenForRedisPublish.js
+++ worker/src/handlers/statement/listenForRedisPublish.js
@@ -39,7 +39,7 @@ export default () => {
             cb();
           });
         },
-        () => !latestResult,
+        async () => !latestResult,
         () => {
           currentlyWorking = false;
         }
